5dfd27b779e7c2745ec6956460cb3af8762ed8cd
Ahmad Awais <me@AhmadAwais.com>
2025-01-27 15:02:36 +0500

    📦 NEW: internal:tests

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+import {Langbase} from 'langbase';
 import welcome from './index.js';
 
 // Use it.
@@ -12,4 +13,26 @@ welcome({
 	version: `1.0`
 });
 
-console.log(`\n\n\n\n\n`);
+// Demo: First shows the welcome header and then an error message or an AI response.
+// This shows the welcome header works no matter what.
+(async () => {
+	try {
+		const langbase = new Langbase({
+			apiKey: process.env.LANGBASE_API_KEY
+		});
+
+		const response = await langbase.pipe.run({
+			name: 'ai-agent',
+			messages: [
+				{
+					role: 'user',
+					content: 'What is Langbase.com?'
+				}
+			]
+		});
+
+		console.log(response);
+	} catch (err) {
+		console.log(err);
+	}
+})();
